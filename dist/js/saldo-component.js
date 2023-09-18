48a4ac46931161d0df0443bac4d00f8018123a4e
Erick de Brito Melo <erick.melo@sorocaba.sp.gov.br>
2023-09-18 18:35:03 -0300

    Code reuse com funções

--- dist/js/saldo-component.js
+++ dist/js/saldo-component.js
@@ -3,14 +3,9 @@ const elementoSaldo = document.querySelector(".saldo-valor .valor");
 const elementoDataAcesso = document.querySelector(".block-saldo time");
 // opção ao "as HTMLElement" acima para resolver o erro de null do TS.
 if (elementoSaldo != null) {
-    elementoSaldo.textContent = saldo.toLocaleString("pt-br", { currency: "BRL", style: "currency" });
+    elementoSaldo.textContent = formatarMoeda(saldo);
 }
 if (elementoDataAcesso != null) {
     const dataAcesso = new Date();
-    elementoDataAcesso.textContent = dataAcesso.toLocaleDateString("pt-br", {
-        weekday: "long",
-        day: "2-digit",
-        month: "2-digit",
-        year: "numeric"
-    });
+    elementoDataAcesso.textContent = formatarData(dataAcesso);
 }
