6d3c0e7a87bfef5c0752996fbb193b38e246c663
Erick de Brito Melo <erick.melo@sorocaba.sp.gov.br>
2023-09-20 12:48:50 -0300

    Tornando as informações de saldo acessíveis (+)
    Bugfix: adicionado type="module" no main.js

--- dist/js/components/saldo-component.js
+++ dist/js/components/saldo-component.js
@@ -5,10 +5,17 @@ let saldo = 3000;
 const elementoSaldo = document.querySelector(".saldo-valor .valor");
 const elementoDataAcesso = document.querySelector(".block-saldo time");
 // opção ao "as HTMLElement" acima para resolver o erro de null do TS.
-if (elementoSaldo != null) {
-    elementoSaldo.textContent = formatarMoeda(saldo);
-}
 if (elementoDataAcesso != null) {
     const dataAcesso = new Date();
     elementoDataAcesso.textContent = formatarData(dataAcesso, FormatoData.DIA_SEMANA_DIA_MES_ANO);
+}
+export function getSaldo() {
+    return saldo;
+}
+atualizarSaldo(saldo);
+export function atualizarSaldo(novoSaldo) {
+    saldo = novoSaldo;
+    if (elementoSaldo != null) {
+        elementoSaldo.textContent = formatarMoeda(saldo);
+    }
 }
--- src/components/saldo-component.ts
+++ src/components/saldo-component.ts
@@ -7,11 +7,20 @@ let saldo:number = 3000;
 const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
 const elementoDataAcesso = document.querySelector(".block-saldo time") as HTMLElement;
 // opção ao "as HTMLElement" acima para resolver o erro de null do TS.
-if (elementoSaldo != null) {
-    elementoSaldo.textContent = formatarMoeda(saldo);
-}
 
 if (elementoDataAcesso != null){
     const dataAcesso: Date = new Date();
     elementoDataAcesso.textContent = formatarData(dataAcesso, FormatoData.DIA_SEMANA_DIA_MES_ANO);
+}
+
+export function getSaldo(): number {
+    return saldo;
+}
+
+atualizarSaldo(saldo);
+export function atualizarSaldo(novoSaldo: number): void {
+    saldo = novoSaldo;
+    if (elementoSaldo != null) {
+        elementoSaldo.textContent = formatarMoeda(saldo);
+    }
 }
