5eac1cc2f8653d3d37d848e4c15a1c9ce825455d
Erick de Brito Melo <erick.melo@sorocaba.sp.gov.br>
2023-09-21 12:41:14 -0300

    Centralizando a lógica de saldo e operações

--- dist/js/components/saldo-component.js
+++ dist/js/components/saldo-component.js
@@ -1,21 +1,22 @@
 import { formatarMoeda } from "../utils/formatters.js";
 import { formatarData } from "../utils/formatters.js";
 import { FormatoData } from "../types/FormatoData.js";
-let saldo = 3000;
+import Conta from "../types/Conta.js";
 const elementoSaldo = document.querySelector(".saldo-valor .valor");
 const elementoDataAcesso = document.querySelector(".block-saldo time");
 // opção ao "as HTMLElement" acima para resolver o erro de null do TS.
 if (elementoDataAcesso != null) {
-    const dataAcesso = new Date();
-    elementoDataAcesso.textContent = formatarData(dataAcesso, FormatoData.DIA_SEMANA_DIA_MES_ANO);
+    elementoDataAcesso.textContent = formatarData(Conta.getDataAcesso(), FormatoData.DIA_SEMANA_DIA_MES_ANO);
 }
-export function getSaldo() {
-    return saldo;
-}
-atualizarSaldo(saldo);
-export function atualizarSaldo(novoSaldo) {
-    saldo = novoSaldo;
+renderizarSaldo();
+function renderizarSaldo() {
     if (elementoSaldo != null) {
-        elementoSaldo.textContent = formatarMoeda(saldo);
+        elementoSaldo.textContent = formatarMoeda(Conta.getSaldo());
     }
 }
+const SaldoComponent = {
+    atualizar() {
+        renderizarSaldo();
+    }
+};
+export default SaldoComponent;
